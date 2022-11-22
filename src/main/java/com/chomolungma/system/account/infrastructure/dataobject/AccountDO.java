a6a72cdc9b8aa542b779c8bb11c7529676f93ef1
kushu001 <yj_tao123@sina.com>
2022-11-22 16:28:08 +0800

    [enhancement] 账户绑定人员字段为null时，无法更新的问题处理

--- src/main/java/com/chomolungma/system/account/infrastructure/dataobject/AccountDO.java
+++ src/main/java/com/chomolungma/system/account/infrastructure/dataobject/AccountDO.java
@@ -1,5 +1,7 @@
 package com.chomolungma.system.account.infrastructure.dataobject;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.chomolungma.core.dataobject.BaseDO;
 import lombok.Data;
@@ -8,6 +10,7 @@ import lombok.Data;
 @TableName("sys_account")
 public class AccountDO extends BaseDO {
     private String username;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long userId;
     private String nickname;
     private String password;
