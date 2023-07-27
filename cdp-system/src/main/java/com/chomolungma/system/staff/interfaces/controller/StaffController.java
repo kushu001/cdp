d42366ff76e0881d617ffc70da8a7027d38bd056
kushu001 <yj_tao123@sina.com>
2023-07-27 20:26:08 +0800

    [bugfix] (Staff) 人员查员bug

--- cdp-system/src/main/java/com/chomolungma/system/staff/interfaces/controller/StaffController.java
+++ cdp-system/src/main/java/com/chomolungma/system/staff/interfaces/controller/StaffController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
